42b24fdf0a1772af3e02bc63239a4e437a833244
ThuanNguyen08 <cntt44dnguyenvanthuan0990@gmail.com>
2024-05-31 11:39:49 +0700

    update migration

--- webAPI/QLNH_APIs/QLNH_APIs/obj/Debug/net8.0/QLNH_APIs.AssemblyInfo.cs
+++ webAPI/QLNH_APIs/QLNH_APIs/obj/Debug/net8.0/QLNH_APIs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLNH_APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8365d285098b6d0c5f467c7b2bd5c476b80ab5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94fe8bc8ca417a8f22b34bba7caf1251b8ab90e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLNH_APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLNH_APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
